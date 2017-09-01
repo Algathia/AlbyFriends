e6500508339f9cf5537296e4d3a19dabd196e7b6
Vialonyx <pro.vialonyx@gmail.com>
2017-09-01 13:15:34 +0200

    formattedAcceptMessage & formattedRefuseMessage

--- src/main/java/fr/algathia/albyfriends/FriendManager.java
+++ src/main/java/fr/algathia/albyfriends/FriendManager.java
@@ -2,6 +2,9 @@ package fr.algathia.albyfriends;
 
 import fr.algathia.albyfriends.commands.CommandResponsePattern;
 import fr.algathia.albyfriends.protocol.packet.FriendRequestPacket;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import java.util.Arrays;
 import java.util.UUID;
@@ -27,6 +30,22 @@ public class FriendManager {
 
         new FriendRequestPacket().send(fromUUID, AlbyFriends.get().getProxy().getPlayer(targetName).getUniqueId().toString());
 
+    }
+
+    public TextComponent getFormattedAcceptMessage(String requestID){
+        TextComponent message = new TextComponent("[ACCEPTER]");
+        message.setColor(ChatColor.GREEN);
+        message.setBold(true);
+        message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/friend accept " + requestID));
+        return message;
+    }
+
+    public TextComponent getFormattedRefuseMessage(String requestID){
+        TextComponent message = new TextComponent("[REFUSER]");
+        message.setColor(ChatColor.RED);
+        message.setBold(true);
+        message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/friend refuse " + requestID));
+        return message;
     }
 
 }
