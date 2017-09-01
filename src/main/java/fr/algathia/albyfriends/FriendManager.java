0c8a4de6a379f5a65f99de3ab1170b4852c81430
Vialonyx <pro.vialonyx@gmail.com>
2017-09-01 13:40:29 +0200

    Implemented request uniques IDs

--- src/main/java/fr/algathia/albyfriends/FriendManager.java
+++ src/main/java/fr/algathia/albyfriends/FriendManager.java
@@ -6,14 +6,19 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import java.util.Arrays;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author Vialonyx
  */
 
 public class FriendManager {
+
+    private List<String> usedRequestsIDs;
+
+    public FriendManager(){
+        this.usedRequestsIDs = new ArrayList<>();
+    }
 
     public void sendFriendRequest(UUID fromUUID, String targetName){
 
@@ -31,6 +36,28 @@ public class FriendManager {
         new FriendRequestPacket().send(fromUUID, AlbyFriends.get().getProxy().getPlayer(targetName).getUniqueId().toString());
 
     }
+
+    // -- Requests IDs management methods --
+
+    private String generateRequestID(String fromName, String targetName){
+        String key = this.getFirstCaracters(fromName) +"-"+ this.getFirstCaracters(targetName);
+        this.usedRequestsIDs.add(key);
+        AlbyFriends.get().getLogger().info("(DEBUG) Data added to Request IDs " + key);
+        return key;
+    }
+
+    private String getFirstCaracters(String s){
+        return s.substring(3, Math.min(s.length(), 3));
+    }
+
+    public boolean requestExists(String fromName, String targetName){
+        if(this.usedRequestsIDs.contains(this.getFirstCaracters(fromName)+"-"+this.getFirstCaracters(targetName))){
+            return true;
+        }
+        return false;
+    }
+
+    // -- Fromatted messages methods --
 
     public TextComponent getFormattedAcceptMessage(String requestID){
         TextComponent message = new TextComponent("[ACCEPTER]");
